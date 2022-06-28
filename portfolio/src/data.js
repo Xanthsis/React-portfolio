b2e840e828492f376b3f225ffa01d9352ddc9058
Suchir <dwarasuchir@gmail.com>
2022-06-28 23:54:13 +0530

    first commit

---
+++ portfolio/src/data.js
@@ -0,0 +1,43 @@
+export const projects = [
+    {
+      title: "Dev Food",
+      subtitle: "Vanilla JS, API",
+      description:
+        "Find your Meal. Find you food by entering the ingredient and on click of button. Food presented to you at your fingertips.",
+      image: "./project-1.gif",
+      link: "https://xanthsis.github.io/RecipeSite/",
+    },
+    {
+      title: "Weather App",
+      subtitle: "React JS",
+      description:
+        "Find the weather forecast of your city!. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://xanthsis.github.io/Weather-Site/",
+    },
+    // {
+    //   title: "Chat App",
+    //   subtitle: "React and Firebase",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-3.gif",
+    //   link: "https://jsbootcamp.com",
+    // },
+    // {
+    //   title: "Epic Todo App",
+    //   subtitle: "React Hooks",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-4.gif",
+    //   link: "https://pythonbootcamp.com",
+    // },
+  ];
+
+  export const skills = [
+    ["C++"],
+    ["HTML"],
+    ["CSS"],
+    ["Javascript"],
+    ["ReactJS"],
+    ["NodeJS"]
+  ]
